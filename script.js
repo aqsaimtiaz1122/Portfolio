8e6d6a680cc8a16a6bf3041c8cbe9733cc7c9ec6
aqsaimtiaz1122 <ishraqimtayaz@gmail.com>
2026-05-03 20:50:14 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-// Day 4: JavaScript Start! 🚀
-// Contact Me Button Working Karo
-
 const contactButton = document.getElementById('contactBtn');
 
 contactButton.addEventListener('click', function(event) {
-    event.preventDefault(); 
+    event.preventDefault();
     alert('Opening your email app...');
-window.location.href = 'mailto:imtiazaqsa623@gmail.com';
+    window.location.href = 'mailto:aqsaimtiaz1122@gmail.com';
+});
 
 console.log('JavaScript loaded! Day 4 Start! 💙');
